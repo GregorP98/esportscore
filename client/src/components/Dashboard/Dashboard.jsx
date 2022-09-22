448590b92bc4d38dda11bdc507aa7c915af2c495
Gregor Pearce <gregorjpearce@gmail.com>
2022-09-22 16:57:33 +0100

    just trying to get some front end for now

---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header.jsx";
+// import Navbar from "../Navbar/Navbar.jsx";
+import "./Dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div id="dashboard">
+      <Header />
+      {/* <Navbar /> */}
+    </div>
+  );
+}
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+// import Logo from "../";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+export default function Header() {
+  return (
+    <div id="navbar">
+      {/* <img id="logo" src={Logo} alt="logo" /> */}
+      <div id="search-container">
+        <FontAwesomeIcon id="search-icon" icon={faMagnifyingGlass} />
+        <input id="searchbar" type="search" placeholder="Search"></input>
+      </div>
+    </div>
+  );
+}
